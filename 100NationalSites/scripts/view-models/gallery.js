976b6a63405cad4fa3d45970b02517bc2e5fa15e
Yordan Bankov <bankoff@mail.bg>
2014-09-28 15:33:27 +0300

    Display image from DB

--- 100NationalSites/scripts/view-models/gallery.js
+++ 100NationalSites/scripts/view-models/gallery.js
@@ -5,14 +5,29 @@ app.viewmodels = app.viewmodels || {};
 (function (scope) {
     'use strict';
 
+    var everlive = new Everlive("lM00QFqWjFjy7gZB");
+
     scope.gallery = kendo.observable({
         title: 'Gallery',
         options: new kendo.data.DataSource({
             data: [{ id: 1, name: 'All' }, { id: 2, name: 'Visited' }, { id: 3, name: 'Not visited' }]
         }),
-        ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'NDK' }, { id: 2, name: 'Al. Nevski' }, { id: 3, name: 'Telerik' }]
-        }),
+        ds: function () {
+            everlive.Files.get().then(function (data) {
+                var files = [];
+                data.result.forEach(function (image) {
+                    files.push(image.Uri);
+                });
+
+                $("#images").kendoMobileListView({
+                    dataSource: files,
+                    template: "<img src='#: data #' style='width:100%'/><span>#: data #</span>"
+                });
+            });
+        },
+        //ds: new kendo.data.DataSource({
+        //    data: [{ id: 1, name: 'NDK' }, { id: 2, name: 'Al. Nevski' }, { id: 3, name: 'Telerik' }]
+        //}),
         alert: function (e) {
             alert(e.data.name);
         }
