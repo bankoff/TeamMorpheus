8a8b07a3dc6dde76e9a343794bb4259253790100
etcet1 <e_grekov@abv.bg>
2014-09-29 03:33:11 +0300

    Geolocation phots almost done

--- 100NationalSites/scripts/view-models/add.js
+++ 100NationalSites/scripts/view-models/add.js
@@ -1,50 +1,75 @@
 /* global kendo, window */
 
 var app = app || {};
-app.viewmodels = app.viewmodels || {};
 
-(function (scope) {
+app.Add = (function () {
     'use strict';
 
-    var everlive = new Everlive("lM00QFqWjFjy7gZB");
+    var addViewModel = (function () {
 
-    scope.add = kendo.observable({
-        title: 'Add',
-        ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'NDK' }, { id: 2, name: 'Al. Nevski' }, { id: 3, name: 'Telerik' }]
-        }),
-        alert: function (e) {
-            alert(e.data.name);
-        },
-        addImage: function() {
+        var isLocationValidSite = function (location) {
+            var query = new Everlive.Query();
+            query.where().nearSphere('Location', [location.longitude, location.latitude], 0.5, 'km');
+            var sites = app.everlive.data('Locations');
+            sites.get(query)
+                .then(function (data) {
+                    console.log(JSON.stringify(data.result));
+                },
+                function (error) {
+                    console.log(JSON.stringify(error));
+                });
+        }
 
-            if (isConnected()) {
-                var success = function(data) {
+        var currentLocation = function () {
+            var options = {
+                enableHighAccuracy: true
+            },
+            currentPosition = {};
 
-                    navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
-                    // onSuccess Geolocation
-                    //
-                    function onSuccess(position) {
-                        alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude);
-                    }
+            var locator = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 
-                    // onError Callback receives a PositionError object
-                    //
-                    function onError(error) {
-                        alert('code: ' + error.code + '\n' +
-                            'message: ' + error.message + '\n');
-                    }
+            // onSuccess Geolocation
+            function onSuccess(position) {
+                //alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude);
+                currentPosition.latitude = position.coords.latitude;
+                currentPosition.longitude = position.coords.longitude;
 
-                    everlive.Files.create({
+                return true;
+            }
+
+            // onError Callback receives a PositionError object
+            function onError(error) {
+                alert('code: ' + error.code + '\n' +
+                    'message: ' + error.message + '\n');
+                return false;
+            }
+
+            if (locator) {
+                return currentPosition;
+            } else {
+                return false;
+            }
+        }
+
+        var addPhotoDetailsToDB = function () {
+
+        };
+
+        var addPhoto = function () {
+            if (app.isConnected()) {
+                var location = currentLocation();
+                isLocationValidSite(location);
+                var success = function (data) {
+                    app.everlive.Files.create({
                         Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
                         ContentType: "image/jpeg",
                         base64: data
                     });
                 };
 
-                var error = function() {
-                    navigator.notification.alert("Unfortunately we could not add the image");
+                var error = function () {
+                    app.showAlert("Unfortunately we could not add the image");
                 };
 
                 var config = {
@@ -56,47 +81,12 @@ app.viewmodels = app.viewmodels || {};
                 navigator.camera.getPicture(success, error, config);
             }
         }
-    });
 
-    function isConnected() {
-        var networkState = navigator.connection.type;
+        return {
+            addPhoto: addPhoto
+        };
 
-        var states = {};
-        //states[Connection.UNKNOWN] = 'Unknown connection';
-        //states[Connection.ETHERNET] = 'Ethernet connection';
-        //states[Connection.WIFI] = 'WiFi connection';
-        //states[Connection.CELL_2G] = 'Cell 2G connection';
-        //states[Connection.CELL_3G] = 'Cell 3G connection';
-        //states[Connection.CELL_4G] = 'Cell 4G connection';
-        states[Connection.NONE] = 'No network connection';
+    }());
 
-        if (states[networkState] === states[Connection.NONE]) {
-            alert(states[networkState]);
-            return false;
-        }
-
-        return true;
-    }
-
-    //scope.add = kendo.observable({
-    //    title: 'Add',
-    //    ds: new kendo.data.DataSource({
-    //        data: [{ id: 1, name: 'NDK' }, { id: 2, name: 'Al. Nevski' }, { id: 3, name: 'Telerik' }]
-    //    }),
-    //    alert: function (e) {
-    //        alert(e.data.name);
-    //    }
-    //});
-
-    //scope.add = kendo.observable({
-    //    title: '',
-    //    isUrgent: false,
-    //    saveTodo: function () {
-    //        //backend serves
-    //        window.todos.push({
-    //            title: this.get('title'),
-    //            isUrgent: this.get('isUrgent')
-    //        });
-    //    }
-    //});
-}(app.viewmodels));
+    return addViewModel;
+}());
